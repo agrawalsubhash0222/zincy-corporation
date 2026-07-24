a4475a5c256a1e12b0666a065bf9ad3852479553
Subbu <subbu@example.com>
2026-07-25 00:25:43 +0530

    Polish local Docker environment and CORS

--- backend/src/main/java/com/zincycorporation/controller/auth/AuthTwilio.java
+++ backend/src/main/java/com/zincycorporation/controller/auth/AuthTwilio.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,7 +16,6 @@ import com.zincycorporation.service.otp.OtpServiceTwilio;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin("*")
 public class AuthTwilio {
 
     @Autowired
